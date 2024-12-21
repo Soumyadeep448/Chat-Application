29e9d70183ea10bdeede7cb187c9fb7e638cf483
Soumyadeep Bhattacharya <sbhattacharya448@gmail.com>
2024-12-21 20:46:18 +0530

    CORS error fixed

--- js/client.js
+++ js/client.js
@@ -5,16 +5,17 @@ const messageInput=document.getElementById('messageInp')
 const messageContainer=document.querySelector(".container")
 
 const append =(message,position)=>{
-    const messageElement=document.createElement('div')
+    console.log('Appedning');
+    const messageElement=document.createElement('div');
     messageElement.innerText=message;
     messageElement.classList.add('message');
     messageElement.classList.add(position);
     messageContainer.append(messageElement);
-}
+};
 
 const name2=prompt("Enter your name to Join");
 socket.emit('new-user-joined', name2); //new-user-joined event from index.js.
 
-socket.on('user-joined',name3=>{
-    append(`${name3} joined the chat`,'right');
+socket.on('user-joined',name=>{
+    append(`${name} joined the chat`,'right');
 })
--- nodeServer/index.js
+++ nodeServer/index.js
@@ -1,16 +1,25 @@
 //Node server which will handle socket.io to connections.
-const io=require('socket.io')(8000)
+const io=require('socket.io')(8000,{
+    cors: {
+        origin: ["http://localhost:5500", "http://127.0.0.1:5500"],  
+        methods: ["GET", "POST"],
+        allowedHeaders: ["Content-Type"],
+        credentials: true
+    }
+});
 
 const users={};
-
+console.log('Connected to the server');
 io.on('connection',socket=>{
+    console.log('Connected to the server');
     socket.on('new-user-joined',name=>{
         console.log("New user",name);
-        users[socket.id]=name;
-        socket.broadcast.emit('user-joined',name);
+            users[socket.id]=name;
+            socket.broadcast.emit('user-joined',name);
     })
 
     socket.on('send',message=>{
-        socket.broadcast.emit('receive',{message: message,name: user[socket.id]})
+        console.log('Message received');
+        socket.broadcast.emit('receive',{message: message,name: users[socket.id]})
     });
 });
